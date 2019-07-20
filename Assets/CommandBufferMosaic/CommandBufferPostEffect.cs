eff645be8fb9a83fc5205a1f003231f85eaf675c
Takashi Irita <irt.tks0311@gmail.com>
2019-07-20 18:03:34 +0900

    add how to mosaic for commandbuffer and Stencilbuffer

---
+++ Assets/CommandBufferMosaic/CommandBufferPostEffect.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+[RequireComponent(typeof(Camera))]
+public class CommandBufferPostEffect : MonoBehaviour
+{
+
+    //イメージエフェクトかけたいshader
+    [SerializeField]
+    private Shader _shader;
+
+    private void Awake()
+    {
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        var camera = GetComponent<Camera>();
+
+        var material = new Material(_shader);
+
+        //コマンドバッファのインスタンス化
+        var commandBuffer = new CommandBuffer();
+        commandBuffer.name = "mosaic";
+
+        //今の地点でのレンダリング結果を一時的にRender Textureへコピー
+        int tempTextureIdentifier = Shader.PropertyToID("_PostEffect");
+        commandBuffer.GetTemporaryRT(tempTextureIdentifier, -1, -1);
+        commandBuffer.Blit(BuiltinRenderTextureType.CameraTarget, tempTextureIdentifier);
+
+        //今の時点でのレンダリング結果をイメージエフェクトかけたいマテリアルに反映させる
+        commandBuffer.Blit(tempTextureIdentifier, BuiltinRenderTextureType.CameraTarget, material);
+
+        // 一時的なレンダーテクスチャを解放
+        commandBuffer.ReleaseTemporaryRT(tempTextureIdentifier);
+
+        // commandBuffer.Blit(BuiltinRenderTextureType.CameraTarget, BuiltinRenderTextureType.CameraTarget, material);
+
+        //コマンドバッファを追加したい場所を登録
+        camera.AddCommandBuffer(CameraEvent.BeforeImageEffects, commandBuffer);
+    }
+}
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # MosaicSpecificObject
 
 ![output](https://user-images.githubusercontent.com/44670044/60755747-28cde500-a02f-11e9-9632-f3b4e1e59f8c.gif)
-© UTJ/UCL
+![mg62](https://user-images.githubusercontent.com/44670044/61576072-ebc62000-ab0f-11e9-9894-c10b49a88b87.gif)
 
 ## Version
 
@@ -9,8 +9,17 @@
 
 ## 内容
 
-特定オブジェクトのみポストエフェクトでモザイクをかけています。Shader 次第で多彩な表現が可能です。
+1. 特定オブジェクトのみ(scene 内ではユニティちゃん全体)ポストエフェクトをかけて、モザイクをかけています。
+2. コマンドバッファ＋ステンシルバッファを使用して、ユニティちゃんのスキンのみにモザイクをかけています。
 
 ## 解説
 
 -[【Unity】ポストエフェクトで特定のオブジェクトにモザイクをかけてみた](https://gurutaka-log.com/unity-post-effect-mosaic)
+
+## 使用 Asset
+
+- ユニティちゃん ©UTJ/UCL
+
+## ライセンス
+
+MIT
